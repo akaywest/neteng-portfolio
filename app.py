3c892ab094be15f7633fe1e3d5c7f5f6c59bffe4
akaywest <akinolakayode40@gmail.com>
2026-07-31 15:01:38 +0100

    Initial commit: Added Flask app and HTML template

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This looks for index.html inside the /templates folder
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # Runs the local development server
+    app.run(debug=True, port=5000)
+    
